20f9ce89e8d66ec6df6ebe53635db9d24f6a643a
zemanue <manureco.97@gmail.com>
2024-10-09 14:29:34 +0200

    Update and fix FilePractica.java
    - Added more lines to learn about creating objects of the FileWriter and PrintWriter classes
    - Paths are fixed

--- src/FilePractica.java
+++ src/FilePractica.java
@@ -1,15 +1,28 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FilePractica {
     public static void main(String[] args) throws IOException {
 
-        File archivo = new File("PracticaAccesoADatos\\resources/Prueba Texto");
+        File archivo = new File("resources\\Prueba Texto 1.txt");
         if (!archivo.exists()) {
             archivo.createNewFile();
         }
 
         System.out.println("Nombre del archivo: " + archivo.getName());
+
+        //CREAR UN PRINTWRITER PARA ESCRIBIR:
+        // Crear a partir de un FileWriter
+        FileWriter fw = new FileWriter("resources/Prueba Texto 2.txt");
+        PrintWriter pw = new PrintWriter(fw);
+
+        //Crear a partir de un objeto File
+        PrintWriter pw2 = new PrintWriter(archivo);
+
+        //Crear a partir de una ruta (si no existe el fichero, se crea)
+        PrintWriter pw3 = new PrintWriter("resources/Prueba Texto 3.txt");
 
     }
 }
