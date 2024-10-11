84d2e7844cf8409d4fd36176285d87d01074be70
zemanue <manureco.97@gmail.com>
2024-10-11 11:08:21 +0200

    Update FilePractica.java
    Added notes to write text with the FileWriter and PrintWriter class

--- src/FilePractica.java
+++ src/FilePractica.java
@@ -18,11 +18,27 @@ public class FilePractica {
         FileWriter fw = new FileWriter("resources/Prueba Texto 2.txt");
         PrintWriter pw = new PrintWriter(fw);
 
-        //Crear a partir de un objeto File
-        PrintWriter pw2 = new PrintWriter(archivo);
-
         //Crear a partir de una ruta (si no existe el fichero, se crea)
-        PrintWriter pw3 = new PrintWriter("resources/Prueba Texto 3.txt");
+        PrintWriter pw2 = new PrintWriter("resources/Prueba Texto 3.txt"); 
 
+        //Crear a partir de un objeto File
+        PrintWriter pw3 = new PrintWriter(archivo);
+        pw3.close();       
+        
+        // Escribir con FileWriter:
+        
+        fw.write("Hola desde FileWriter. ");
+        fw.write("Con el método write(), puedo escribir cadenas de texto y encadenar variables y expresiones. ");
+        String nombre = "Usuario";
+        fw.write("Por ejemplo, puedo escribir 'Hola, Mundo' encadenando este String con el String 'nombre' así: hola, " + nombre + ".");
+        fw.close(); // Siempre cerrar el FileWriter
+
+        // Escribir con PrintWriter:
+        pw2.println("Hola desde PrintWriter. ");
+        pw2.println("Con el método println(), puedo escribir cadenas de texto, variables y expresiones por líneas.");
+        pw2.println("También se pueden utilizar para escribir los métodos print() y printf().");
+        pw2.println("Con el método flush(), se escribe en el archivo las líneas que haya almacenado en el buffer.");
+        pw2.flush(); // Escribir lo que hay almacenado en el buffer
+        pw2.close(); // Siempre cerrar el PrintWriter
     }
 }
