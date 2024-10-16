7e50db077e2594391958b40090168bb266aa5177
zemanue <manureco.97@gmail.com>
2024-10-16 19:08:40 +0200

    Update FicherosTarea2.java
    Added new comments that explain what the code does

--- src/FicherosTarea2.java
+++ src/FicherosTarea2.java
@@ -10,15 +10,17 @@ import java.util.Scanner;
 
 public class FicherosTarea2 {
     public static void main(String[] args) throws IOException {
+        // Creamos el objeto File con la ruta del archivo
         File datosTxt = new File("resources\\datos.txt");
 
+        // Creamos un objeto Scanner para leer el archivo
         Scanner sc = new Scanner(datosTxt);
         int numPalabras = 0;
-        while (sc.hasNext()) {
-            numPalabras++;
-            sc.next();
+        while (sc.hasNext()) { // Si más hay palabras en el archivo...
+            numPalabras++; // Se incrementa el contador
+            sc.next(); // El método next() lee la siguiente palabra
         }
-        
+
         System.out.println("El archivo tiene " + numPalabras + " palabras.");
         sc.close();
     }
