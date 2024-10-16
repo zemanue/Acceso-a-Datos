c24d7af3b5a5ebb502ee752fe3caf64750043cc9
zemanue <manureco.97@gmail.com>
2024-10-16 18:53:11 +0200

    Format FicherosTarea1.java
    Document formatted

--- src/FicherosTarea1.java
+++ src/FicherosTarea1.java
@@ -1,3 +1,13 @@
+/*
+* TAREA 1
+Escribe un programa Java que realice las siguientes acciones:
+1. Cree un nuevo fichero llamado “datos.txt” en el sistema de archivos.
+2. Escribe en el fichero las siguientes líneas:
+a. Nombre de la asignatura
+b. Nombre del estudiante.
+3. Lea el contenido y muestre por consola el texto almacenado.
+*/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,7 +24,7 @@ public class FicherosTarea1 {
         PrintWriter pw = new PrintWriter(datosTxt);
         pw.println("Nombre de la asignatura: Acceso a Datos");
         pw.println("Nombre del estudiante: José Manuel Redondo Conde");
-        pw.close(); 
+        pw.close();
 
         // Leemos el contenido del archivo usando la clase BufferedReader
         BufferedReader br = new BufferedReader(new FileReader(datosTxt));
@@ -24,7 +34,7 @@ public class FicherosTarea1 {
             while (linea != null) {
                 System.out.println(linea);
                 linea = br.readLine();
-            }    
+            }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } finally {
