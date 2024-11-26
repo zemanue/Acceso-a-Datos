da32f3c328437fa9acc02b06f13ed23cb3a06931
zemanue <manureco.97@gmail.com>
2024-11-26 15:59:05 +0100

    Update Cliente.java and Pedido.java
    private attributes, empty constructor, getters and setter added in both classes. Also Serializable interface implemented.

--- mapeojpa/src/main/java/com/mapeojpa/Cliente.java
+++ mapeojpa/src/main/java/com/mapeojpa/Cliente.java
@@ -1,5 +1,73 @@
 package com.mapeojpa;
 
-public class Cliente {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String idCliente;
+
+    private String nombre;
+    private String apellido;
+    private int telefono;
+    private String email;
+    private String direccion;
+
+    public Cliente() {
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 
 }
--- mapeojpa/src/main/java/com/mapeojpa/Pedido.java
+++ mapeojpa/src/main/java/com/mapeojpa/Pedido.java
@@ -1,5 +1,63 @@
 package com.mapeojpa;
 
-public class Pedido {
+import java.io.Serializable;
+import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido implements Serializable {
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.SEQUENCE)
+    private String idPedido;
+
+    private int totalProductos;
+    private int totalPrecio;
+    private Date fecha;
+    private Cliente cliente;
+
+    public Pedido() {
+    }
+
+    public String getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(String idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getTotalProductos() {
+        return totalProductos;
+    }
+
+    public void setTotalProductos(int totalProductos) {
+        this.totalProductos = totalProductos;
+    }
+
+    public int getTotalPrecio() {
+        return totalPrecio;
+    }
+
+    public void setTotalPrecio(int totalPrecio) {
+        this.totalPrecio = totalPrecio;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
