8726f1a474cdfbe460afd8373011a8bc68e38023
zemanue <manureco.97@gmail.com>
2025-04-06 12:35:27 +0200

    Create Tarea-spring-boot project

---
+++ tarea-spring-boot/src/main/java/com/example/tarea/spring/boot/TareaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarea.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSpringBootApplication.class, args);
+	}
+
+}
---
+++ tarea-spring-boot/src/test/java/com/example/tarea/spring/boot/TareaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
